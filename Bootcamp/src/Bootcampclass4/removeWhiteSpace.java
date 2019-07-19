87ed40fa3d6891efc67ea88675f4abf30807196e
asmitadahal <asmitadahal.physio@gmail.com>
2019-07-19 00:07:15 -0400

    HW

---
+++ Bootcamp/src/Bootcampclass4/removeWhiteSpace.java
@@ -0,0 +1,11 @@
+package Bootcampclass4;
+
+public class removeWhiteSpace {
+	public static void main(String[] args) {
+		String sentence = "T his is b ett er.";
+		System.out.println("Original sentence: " + sentence);
+		sentence = sentence.replaceAll("\\s", "");
+		System.out.println("After replacement: " + sentence);
+		}
+		}
+
---
+++ Bootcamp/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author URMI
+ *
+ */
+module Bootcamp {
+}
